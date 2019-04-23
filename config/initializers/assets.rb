19508f4319ce0dad79646d8b433c81b067bbaee4
Aaron Conklin <conklin.aaron@gmail.com>
2019-04-23 12:40:31 -0400

    removed assets compilation on production, but added it to initializers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,9 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( lightbox/* )
 
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/ 
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.

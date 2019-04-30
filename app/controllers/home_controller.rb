85f21f83c794d2896539b693b8146eb940e480e7
Aaron Conklin <conklin.aaron@gmail.com>
2019-04-30 13:53:43 -0400

    order by date and start time on featured events on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-  	@featured_events = Event.order(date: :asc).select { |e| e.date >= Date.today }.first(4)
+  	@featured_events = Event.order(date: :asc).order(start_time: :asc).select { |e| e.date >= Date.today }.first(4)
   end
 
 end

2d58cd6fb55fd6ac4385d2b19c39d529d0b79ce8
Aaron Conklin <conklin.aaron@gmail.com>
2019-05-02 13:27:46 -0400

    model validation for title and description of staff

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,4 +2,6 @@ class Staff < ApplicationRecord
 	scope :oldest_first, lambda { order(created_at: :asc) }
 
 	mount_uploader :staff_image_id, PhotoUploader
+	
+	validates_presence_of :title, :description
 end

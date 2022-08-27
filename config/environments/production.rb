de94cdbec768a4c8d9211d9bc0f0824ba0c1497b
Aaron Conklin <conklin.aaron@gmail.com>
2022-08-27 14:27:52 -0400

    adding env symbol...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # Allow Classes
+  config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time]
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rockbar_#{Rails.env}"
